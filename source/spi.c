2c2daedbb9c73b8d94019b9ec71e82cade10da3d
fordp2002 <SimonEllwood@hotmail.com>
2015-12-03 19:29:37 +0000

    Tidy up and speed up.

--- source/spi.c
+++ source/spi.c
@@ -31,20 +31,16 @@ void spi_pin_init(void)
 
 void spi_begin(void)
 {	
-	unsigned int var;
 	spi_pin_init();
         
 	SPI0_CONTROL = 0;
 	SPI0_CONTROL |= BIT(SPI_C_CLEAR_RX) | BIT(SPI_C_CLEAR_TX);				// Clear TX and RX fifos
 	
-	var = SPI0_CONTROL;
-	var &= ~(BIT(SPI_C_CPOL) | BIT(SPI_C_CPHA));	 								// Set data mode = 0 
-	var &= ~(BIT(SPI_C_CS0) | BIT(SPI_C_CS1));									// Set to CS0 and Chip Select Polarity=LOW
-	var &= ~BIT(SPI_C_CSPOL0);
+	SPI0_CONTROL &= ~(BIT(SPI_C_CPOL) | BIT(SPI_C_CPHA));	 					// Set data mode = 0
+	SPI0_CONTROL &= ~(BIT(SPI_C_CS0) | BIT(SPI_C_CS1));						// Set to CS0 and Chip Select Polarity=LOW
+	SPI0_CONTROL &= ~BIT(SPI_C_CSPOL0);
 
-	SPI0_CONTROL = var;																	// Write back updated value
-
- 	SPI0_CLK = 1024;       																// Set clock to 250kHz
+ 	SPI0_CLK = 16;       																// Set clock to ???
 }
 
 void spi_transfer(u8* TxBuff, u8* RxBuff, u32 Length)
@@ -79,15 +75,6 @@ void spi_transfer(u8* TxBuff, u8* RxBuff, u32 Length)
 
 void spi_end(void)																		// Set all the SPI0 pins back to input
 {
- 	unsigned int var;
-
-	var = gpioGPFSEL1;																	// Read current value of GPFSEL1- pin 10 & 11
-
-	var &=~((7 << 0) | (7 << 3));														// Set as input pins GPIO = 000
- 	gpioGPFSEL1 =var;																		// Write back updated value
-	
-	var = gpioGPFSEL0;																	// Read current value of GPFSEL1- pin 7,8 & 9
-
-	var &=~((7 << 21) | (7 << 24) | (7 << 27));									// Set as input pins GPIO = 000
- 	gpioGPFSEL1 =var;																		// Write back updated value
+	gpioGPFSEL1 &=~((7 << 0) | (7 << 3));											// Set as input pins GPIO = 000
+	gpioGPFSEL0 &=~((7 << 21) | (7 << 24) | (7 << 27));						// Set as input pins GPIO = 000																// Write back updated value
 }
